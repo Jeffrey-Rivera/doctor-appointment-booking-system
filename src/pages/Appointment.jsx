45a31ed2c755a51e5a6c1d9bc0572ec4dbe03093
Jeffrey-Rivera <jeffreyrivera711@gmail.com>
2024-10-12 13:57:25 -0600

    appointment booking modifications

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -11,20 +11,64 @@ const Appointment = () => {
   const [docInfo, setDocInfo] = useState(null)
   const [docSlots, setDocSlots] = useState([])
   const [slotIndex, setSlotIndex] = useState(0)
-  const [slotTime, setSlotTim] = useState('')
+  const [slotTime, setSlotTime] = useState('')
 
   const fetchDocInfo = async () => {
     const docInfo = doctors.find(doc => doc._id === docId)
     setDocInfo(docInfo)
-    console.log(docInfo)
-
   }
 
+  const getAvailableSlots = async () => {
+    setDocSlots([])
+
+    //current date
+    let today = new Date()
+
+    for (let i = 0; i, 7; i++) {
+      //date with index
+      let currentDate = new Date(today)
+      currentDate.setDate(today.getDate() + i)
+
+      //end time of the date with the index
+      let endTime = new Date()
+      endTime.setDate(today.getDate() + 1)
+      endTime.setHours(20, 0, 0, 0)
+
+      //set hours
+      if (today.getDate() === currentDate.getDate()) {
+        currentDate.setHours(currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10)
+        currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 0)
+      } else {
+        currentDate.setHours(10)
+        currentDate.setMinutes(0)
+      }
+
+      let timeSlots = []
+
+      while (currentDate < endTime) {
+        let formattedTime = currentDate.toLocaleString([], { hour: '2 digit', minute: '2-digit' })
+
+        // add slot for array
+        timeSlots.push({
+          datetime: new Date(currentDate),
+          time: formattedTime
+        })
+
+        //increment from actual time by 30 mins
+        currentDate.setMinutes(currentDate.getMinutes() + 30)
+      }
+    }
+
+  }
 
 
   useEffect(() => {
     fetchDocInfo()
   }, [doctors, docId])
+
+  useEffect(() => {
+    getAvailableSlots
+  }, [docInfo])
 
 
   return docInfo && (
